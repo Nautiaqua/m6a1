14a0239a721aaaa9f0aa76fbcd6d92f7233d6e1d
nautia <Nautia@Carol>
2025-03-18 23:16:52 +0800

    retry button added

--- src/javaapp/resulta.java
+++ src/javaapp/resulta.java
@@ -106,6 +106,7 @@ public void useData() {
         jLabel10 = new javax.swing.JLabel();
         jLabel11 = new javax.swing.JLabel();
         rating = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -267,6 +268,16 @@ public void useData() {
         });
         jPanel2.add(rating, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 170, 150, -1));
 
+        jButton1.setBackground(new java.awt.Color(108, 82, 75));
+        jButton1.setFont(new java.awt.Font("Ebrima", 0, 14)); // NOI18N
+        jButton1.setText("Retry Quiz");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 300, 140, -1));
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -279,13 +290,13 @@ public void useData() {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 331, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 374, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
 
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
-        setSize(new java.awt.Dimension(642, 339));
+        setSize(new java.awt.Dimension(642, 388));
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
@@ -312,6 +323,15 @@ public void useData() {
     private void ratingActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ratingActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_ratingActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        identification.idenSCORE = 0;
+        multichoice.multiSCORE = 0;
+        trueorfalse.TFSCORE = 0;
+        new identification().setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -351,6 +371,7 @@ public void useData() {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     public static javax.swing.JTextField TOTALITYFINAL;
     public static javax.swing.JTextField idenTOTAL;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
